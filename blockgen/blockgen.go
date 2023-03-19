7668fe9e01b22bb4640e17e775c4c063ddd7081f
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-03-19 15:35:22 +0300

    hash nonce

--- blockgen/blockgen.go
+++ blockgen/blockgen.go
@@ -2,6 +2,8 @@ package blockgen
 
 import (
 	"hash"
+	"crypto/sha256"
+	"strconv"
 )
 
 type Block struct {
@@ -13,11 +15,22 @@ type Block struct {
 }
 
 type Data [256]byte
-type Nonce [16]byte
+type Nonce int
 
 func GenerateNextFrom(prev Block) Block {
-	var next = Block{}
+	next := Block{}
 	next.Index = prev.Index + 1
 	next.PrevHash = prev.Hash
+	hash := sha256.New()
+	nonce := 0
+	sum := hash.Sum(nil)
+	for sum[len(sum)-1] % 16 != 0 {
+		hash.Reset()
+		hash.Write([]byte(strconv.Itoa(nonce)))
+		sum = hash.Sum(nil)
+		nonce += 1
+	}
+	next.Hash = hash
+	next.Nonce = Nonce(nonce)
 	return next;
 }
--- blockgen/blockgen_test.go
+++ blockgen/blockgen_test.go
@@ -3,6 +3,7 @@ package blockgen
 import (
 	"crypto/sha256"
 	"testing"
+	"fmt"
 )
 
 func TestGenerateNextFrom_Index(t *testing.T) {
@@ -10,7 +11,7 @@ func TestGenerateNextFrom_Index(t *testing.T) {
 	var next = GenerateNextFrom(prev)
 	var wantIndex = prev.Index + 1
 	if next.Index != wantIndex {
-		t.Errorf("Next block index = %d; want %d", next.Index, wantIndex)
+		t.Errorf("index = %d; want %d", next.Index, wantIndex)
 	}
 }
 
@@ -19,9 +20,20 @@ func TestGenerateNextFrom_PrevHash(t *testing.T) {
 	prev.Hash = sha256.New()
 	prev.Hash.Write([]byte("test"))
 	var next = GenerateNextFrom(prev)
-	var prevSum = string(next.PrevHash.Sum([]byte{}))
-	var wantSum = string(prev.Hash.Sum([]byte{}))
+	var prevSum = string(next.PrevHash.Sum(nil))
+	var wantSum = string(prev.Hash.Sum(nil))
 	if prevSum != wantSum {
-		t.Errorf("Next block: previous hash is incorrect")
+		t.Errorf("previous hash is incorrect")
+	}
+}
+
+func TestGenerateNextFrom_Hash(t *testing.T) {
+	var prev = Block{}
+	var next = GenerateNextFrom(prev)
+	var sum = fmt.Sprintf("%x", next.Hash.Sum(nil))
+	var ending = sum[len(sum)-1:]
+	const want = "0"
+	if ending != want {
+		t.Errorf("got hash ending with %s; want %s", ending, want)
 	}
 }
