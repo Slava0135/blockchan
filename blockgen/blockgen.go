1f4522c614a41c6c35e3de499e84d9e69f083d61
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-03-19 16:16:33 +0300

    minor refactor

--- blockgen/blockgen.go
+++ blockgen/blockgen.go
@@ -18,13 +18,13 @@ type Data [256]byte
 type Nonce int
 
 func GenerateNextFrom(prev Block, data Data) Block {
-	next := Block{}
+	var next = Block{}
 	next.Index = prev.Index + 1
 	next.PrevHash = prev.Hash
 	next.Data = data
-	hash := sha256.New()
-	nonce := 0
-	sum := hash.Sum(nil)
+	var hash = sha256.New()
+	var nonce = 0
+	var sum = hash.Sum(nil)
 	for sum[len(sum)-1] % 16 != 0 {
 		hash.Reset()
 		hash.Write([]byte(strconv.Itoa(nonce)))
