9862d629030b42ed3a74afb8b0d443cc30c3e948
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-03-21 15:16:00 +0300

    rename func

--- validate/validate.go
+++ validate/validate.go
@@ -4,13 +4,13 @@ import (
 	"slava0135/blockchan/blockgen"
 )
 
-func IsChainValid(chain []blockgen.Block) bool {
+func IsValidChain(chain []blockgen.Block) bool {
 	for i, b := range chain {
 		if string(blockgen.CalculateHashFrom(b).Sum(nil)) != string(b.Hash.Sum(nil)) {
 			return false
 		}
 		if i > 0 {
-			if chain[i].Index != chain[i-1].Index + 1 {
+			if chain[i].Index != chain[i-1].Index+1 {
 				return false
 			}
 			if chain[i].PrevHash != chain[i-1].Hash {
--- validate/validate_test.go
+++ validate/validate_test.go
@@ -5,42 +5,42 @@ import (
 	"testing"
 )
 
-func TestIsChainValid_Empty(t *testing.T) {
-	if !IsChainValid(nil) {
+func TestIsValidChain_Empty(t *testing.T) {
+	if !IsValidChain(nil) {
 		t.Errorf("empty chain is not valid")
 	}
 }
 
-func TestIsChainValid_InvalidHash(t *testing.T) {
+func TestIsValidChain_InvalidHash(t *testing.T) {
 	var one = blockgen.GenerateGenesisBlock()
 	one.Nonce += 1
-	if IsChainValid([]blockgen.Block{one}) {
+	if IsValidChain([]blockgen.Block{one}) {
 		t.Errorf("block with different hash is valid")
 	}
 }
 
-func TestIsChainValid_ValidChain(t *testing.T) {
+func TestIsValidChain_ValidChain(t *testing.T) {
 	var gen = blockgen.GenerateGenesisBlock()
 	var chain = []blockgen.Block{gen}
 	for i := byte(0); i < 10; i += 1 {
 		chain = append(chain, blockgen.GenerateNextFrom(chain[i], blockgen.Data{}))
 	}
-	if !IsChainValid(chain) {
+	if !IsValidChain(chain) {
 		t.Errorf("valid chain is invalid")
 	}
 }
 
-func TestIsChainValid_PrevHashNotMatching(t *testing.T) {
+func TestIsValidChain_PrevHashNotMatching(t *testing.T) {
 	var one = blockgen.GenerateGenesisBlock()
 	var two = blockgen.GenerateNextFrom(one, blockgen.Data{})
 	one.Data[0] += 1
 	one.Hash = blockgen.CalculateHashFrom(one)
-	if IsChainValid([]blockgen.Block{one, two}) {
+	if IsValidChain([]blockgen.Block{one, two}) {
 		t.Errorf("block with invalid prev hash is valid")
 	}
 }
 
-func TestIsChainValid_WrongIndex(t *testing.T) {
+func TestIsValidChain_WrongIndex(t *testing.T) {
 	var gen = blockgen.GenerateGenesisBlock()
 	var chain = []blockgen.Block{gen}
 	for i := byte(0); i < 10; i += 1 {
@@ -51,7 +51,7 @@ func TestIsChainValid_WrongIndex(t *testing.T) {
 			chain[mut].Hash = blockgen.CalculateHashFrom(chain[mut])
 		}
 	}
-	if IsChainValid(chain) {
+	if IsValidChain(chain) {
 		t.Errorf("block with invalid index is valid")
 	}
 }
