ede3dc0f069bf03c3008b4ba76176f600bfca916
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-03-25 21:11:37 +0300

    3 nodes at time

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
-    var self = network.Remote{Address: ":8888"}
-    network.Launch(":8888", []network.Remote{self})
+    var remotes = []network.Remote{{Address: ":8888"}, {Address: ":8889"}, {Address: ":8890"}}
+    go network.Launch(remotes[0].Address, []network.Remote{remotes[1], remotes[2]})
+    go network.Launch(remotes[1].Address, []network.Remote{remotes[0], remotes[2]})
+    network.Launch(remotes[2].Address, []network.Remote{remotes[0], remotes[1]})
 }
