4f3418c077b285256097b1571db3c7212323184c
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-03-28 13:26:51 +0300

    move network.go in main package

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"flag"
-	"slava0135/blockchan/network"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-    name := flag.String("name", "NONAME", "node name for logging")
-    genesis := flag.Bool("genesis", false, "generate genesis block on start?")
-    addr := flag.String("addr", "localhost:8888", "address node will be running")
-    debug := flag.Bool("debug", false, "enable debug level logging")
-    flag.Parse()
-    if *debug {
-        log.SetLevel(log.DebugLevel)
-    }
-    remotes := flag.Args()
-    network.Launch(*name, *addr, remotes, *genesis)
+	name := flag.String("name", "NONAME", "node name for logging")
+	genesis := flag.Bool("genesis", false, "generate genesis block on start?")
+	addr := flag.String("addr", "localhost:8888", "address node will be running")
+	debug := flag.Bool("debug", false, "enable debug level logging")
+	flag.Parse()
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	}
+	remotes := flag.Args()
+	Launch(*name, *addr, remotes, *genesis)
 }
